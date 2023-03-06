2ac132404267261e154fe2cdd98cb64a133c7a5c
Pradeep Vishwakarma <pradeep9340@outlook.com>
2023-03-06 08:50:08 +0530

    first commit

---
+++ New.js
@@ -0,0 +1,29 @@
+// var htt = require('http');
+// htt.createServer((req,resp)=>{
+//     resp.write("<h1 style='color:red;'>Hello this is my server</h1>");
+//     resp.end();
+// }).listen(4500);
+
+
+
+// var http = require('http');
+// function fun(req,resp){
+//     resp.write("<h1 style='text-align:center;'>Hii</h1>");
+//     resp.end();
+// }
+// http.createServer(fun).listen(1111)
+
+// var a = require('http')
+// var c = (req,resp) => {
+//     resp.write("<h1>Hello world</h1>");
+//     resp.end();
+// }
+// a.createServer(c).listen(1111)
+
+
+// var a = require('http');
+// a.createServer((req,resp)=>{
+//     resp.write("My server");
+//     resp.end();
+// }).listen(1111)
+
